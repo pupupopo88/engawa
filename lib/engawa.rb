50eaf1941f60d39549b5062ef0f4b46046d73e61
pupupopo88 <39chenas@gmail.com>
2015-08-24 21:32:51 +0900

    prepare for release

--- lib/engawa.rb
+++ lib/engawa.rb
@@ -1,5 +1,29 @@
 require "engawa/version"
 
 module Engawa
-  # Your code goes here...
+  def aburi
+    'うますぎ'
+  end
+
+  def heavy_stomach
+    '( ･ω･)つ[芽ネギ]'
+  end
+
+  def fish
+    <<-EOS
+    ヒラメ
+    　　　　　　,.. ー''´￣＿.￣｀''- ..,,_
+　　　　 ／,..-ｰ''´￣ 　 ￣｀''- .,,_ ＼
+　 　 ／／　　　　　　　　　　　　 ＼,,＼,
+　 ／／ 　,. - ､ 　　　　　　　　　　　 ＼.>　　,..┐
+　 !／　../　　__ヽ_＿＿_＿_＿_＿_＿_＿二l'´　.|
+<ﾟ｡　. 　 `､'彡ﾂ 　 　 　 　　　　　　　 　 _,,..ｔ,　｜
+ヾ__／　／ｰ ´ 　　 　 　 　 　　　　 　 /ﾞ>　　~'┘
+　＼ー'　　　　　　　　　　　　　　　 ／／
+　　 ｀''-..,,,,_ 　　　　　　　　　 _,.-'" ／
+　　　　　 ヾi｀ヾ''‐‐---‐ー'''´　_,／
+　　　 　 　 ﾞ' 　｀'''''―--‐ー'''´
+    EOS
+  end
+  module_function :aburi, :heavy_stomach, :fish
 end
--- test/engawa_test.rb
+++ test/engawa_test.rb
@@ -8,4 +8,12 @@ class EngawaTest < Minitest::Test
   def test_it_does_something_useful
     assert false
   end
+
+  def test_face
+    assert_equal 'うますぎ', Engawa.aburi
+  end
+
+  def heavy_stomach
+    assert_equal '( ･ω･)つ[芽ネギ]', Engawa.heavy_stomach
+  end
 end
